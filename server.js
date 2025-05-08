cab70ff2d2857d465fc8b346b3fb58b3beb2cf7d
Nithesh <31339538+nitheshurao@users.noreply.github.com>
2025-05-08 08:43:09 +0530

    build fix

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get('/',(req,res)=>{
+  res.send('Hello World');
+})
 app.use('/api/auth', authRoutes);
 app.use('/api/dsa', dsaRoutes);
 
